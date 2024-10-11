bfa4028b144f4e77593cd8414d7fd2ca184c89bc
Antonioo-Wu <anwu@uade.edu.ar>
2024-10-11 15:32:34 -0300

    algunos controles y modelos

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,16 @@
 const AuthController = {
     register: (req, res) => {
-      // Lógica de registro
+      // TODO
       res.status(201).send('Usuario registrado');
     },
     
     login: (req, res) => {
-      // Lógica de login
+      // TODO
       res.status(200).json({ token: 'jwt_token_aqui' });
     },
   
     changePassword: (req, res) => {
-      // Lógica para cambiar la contraseña
+      // TODO
       res.status(200).send('Contraseña actualizada');
     }
   };
